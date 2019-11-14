d179ed09daa36aec3463b76f232b9bece422448e
Miroslaw Chmielecki <misiak19@op.pl>
2019-11-14 17:54:15 +0100

    Server refactoring

---
+++ server/routes/product.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const ProductController = require("../controllers/product.controller");
+
+// get all products
+router.route("/products").get(ProductController.getProducts);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,36 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const config = require("./config");
 
 const app = express();
 
+//import routes
+const productRoutes = require("./routes/product.routes");
+
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/api", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  const data = [
-    {
-      id: "1adfasf",
-      title: "Lorem Ipsum",
-      content:
-        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."
-    },
-    {
-      id: "2evxc34",
-      title: "Lorem Ipsum II",
-      content:
-        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."
-    },
-    {
-      id: "24edxc14",
-      title: "Lorem Ipsum III",
-      content:
-        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."
-    }
-  ];
-  res.json(data);
-});
-
-app.listen(8000, function() {
-  console.log("Server is running on port:", 8000);
+app.listen(config.PORT, function() {
+  console.log("Server is running on port:", config.PORT);
 });
